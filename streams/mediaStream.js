efb80c1df4cdf5506cc0fe8b7aac60dff42a5bd8
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-08-14 23:36:49 +0200

    Stream to the browser

---
+++ streams/mediaStream.js
@@ -0,0 +1,15 @@
+const { createServer } = require("http");
+const { stat, createReadStream } = require("fs");
+const { promisify } = require("util");
+const file = "./install-node.mp4";
+const fileInfo = promisify(stat);
+
+createServer(async (req, res) => {
+  const { size } = await fileInfo(file);
+  console.log("asd");
+  res.writeHead(200, {
+    "Content-Length": size,
+    "Content-Type": "video/mp4",
+  });
+  createReadStream(file).pipe(res);
+}).listen(3000, () => console.log("Server is running - 3000"));
