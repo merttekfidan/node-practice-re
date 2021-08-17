ae3b81004494b1099e21b73da76b8160f11c7516
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-08-17 23:52:07 +0200

    Builder Pattern

--- design patterns/Creational-Patterns/Builder/Person.js
+++ design patterns/Creational-Patterns/Builder/Person.js
@@ -1,18 +1,16 @@
 class Person {
+  constructor(builder) {
+    this.name = builder.name;
+    this.isEmployee = builder.isEmployee;
+    this.isManager = builder.isManager;
+    this.hours = builder.hours || 0;
+    this.money = builder.money || 0;
+    this.shoppingList = builder.shoppingList || [];
+  }
 
-    constructor(name, isEmployee=false, isManager=false, hours=40, money=0, shoppingList=[] ) {
-        this.name = name
-        this.isEmployee = isEmployee;
-        this.isManager = isManager;
-        this.hours = hours;
-        this.money = money;
-        this.shoppingList = shoppingList;
-    }
-
-    toString() {
-        return JSON.stringify(this);
-    }
-
+  toString() {
+    return JSON.stringify(this);
+  }
 }
 
 module.exports = Person;
